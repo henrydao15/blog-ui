73c07876f49fdfb479f1549864ab632c4fda9e3f
Luyen Dao Dinh <91018384+henrydao15@users.noreply.github.com>
2022-07-04 17:21:41 +0700

    homepage
    upload homepage html thuan

---
+++ src/Pages/Hompage/Main.js
@@ -0,0 +1,285 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="Main">
+      <div className="container" id="section1">
+        <div className="row" id="home-img1">
+          <div className="col-lg-6 .col-md-6 .col-sm-6 .col-xs-12">
+            <img src="https://blog.green-acres.com/wp-content/uploads/2018/09/shutterstock_230413204.jpg"
+                 alt="homeimg" />
+          </div>
+
+          <div className="col-lg-6 .col-md-6 .col-sm-6 .col-xs-12" id="icon1">
+            <div>
+              <img src="/homepage/Img/icons8-table-lamp-50.png" alt="icon"/>
+            </div>
+            <p>Hi, we’re Nick & Hannah from Salt in our Hair.
+              We create inspiring travel blogs about the world’s most beautiful places to make planning your
+              holiday a piece of cake!</p>
+            <p>Choose a
+              <select>
+                <option value="default">destination here</option>
+                <option value="1">HN</option>
+                <option value="2">QN</option>
+                <option value="3">HA</option>
+              </select>
+              to start your journey.
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <!-- section2 -->
+      <div className="container" id="section2">
+        <div className="row">
+          <div className="col-lg-4 .col-md-4 .col-sm-6 .col-xs-12">
+            <div className="text-center">
+              <img src="/homepage/Img/icons8-table-lamp-50.png" alt="img1"/>
+                <div>Italy</div>
+            </div>
+            <div className="text-center">
+              <h3>
+                16 Best Things to do in Umbria, Italy
+              </h3>
+            </div>
+            <div className="featured-img">
+              <img src="https://blog.green-acres.com/wp-content/uploads/2018/09/shutterstock_230413204.jpg"
+                   alt="" />
+            </div>
+          </div>
+          <div className="col-lg-4 .col-md-4 .col-sm-6 .col-xs-12">
+            <div className="text-center">
+              <img src="/homepage/Img/icons8-table-lamp-50.png" alt="img1" />
+                <div>Italy</div>
+            </div>
+            <div className="text-center">
+              <h3>
+                16 Best Things to do in Umbria, Italy
+              </h3>
+            </div>
+            <div className="featured-img">
+              <img src="https://blog.green-acres.com/wp-content/uploads/2018/09/shutterstock_230413204.jpg"
+                   alt=""/>
+            </div>
+          </div>
+          <div className="col-lg-4 .col-md-4 .col-sm-6 .col-xs-12">
+            <div className="text-center">
+              <img src="/homepage/Img/icons8-table-lamp-50.png" alt="img1"/>
+                <div>Italy</div>
+            </div>
+            <div className="text-center">
+              <h3>
+                16 Best Things to do in Umbria, Italy
+              </h3>
+            </div>
+            <div className="featured-img">
+              <img src="https://blog.green-acres.com/wp-content/uploads/2018/09/shutterstock_230413204.jpg"
+                   alt="" />
+            </div>
+          </div>
+        </div>
+
+        <!-- section redomendation -->
+        <div className="container">
+          <ul className="where-to">
+            <li>
+              Where to next?
+            </li>
+            <li>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/7/79/Thap_Rua.jpg" alt=""/>Costa rica
+
+            </li>
+          </ul>
+
+          <ul className="list-group">
+            <li className="list-group-item"> Where to next?</li>
+            <li className="list-group-item">A second item</li>
+          </ul>
+        </div>
+      </div>
+
+
+      <!-- section3 -->
+      <div className="container" id="section3">
+        <div className="row banner">
+          <div className="col-lg-6 .col-md-6 .col-sm-12 .col-xs-12">
+            <h3>Its easy to do better</h3>
+            <h1>Sustainable travel</h1>
+            <p>Sustainable travel means traveling slow, with different transport, packing eco-friendly, stay local
+              and much more!</p>
+            <button>Travel tip</button>
+          </div>
+          <div className="col-lg-6 .col-md-6 .col-sm-12 .col-xs-12 section3-right">
+            <div className="banner-right text-center">
+              <div className="row banner-article">
+                <div className="col-3">
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/7/79/Thap_Rua.jpg" alt=""/>
+                </div>
+                <div className="col-9">
+                  <p>13 Easy Tips to Reduce Plastic & Travel Plastic-Free</p>
+                </div>
+              </div>
+              <div className="row banner-article">
+                <div className="col-3">
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/7/79/Thap_Rua.jpg" alt=""/>
+                </div>
+                <div className="col-9">
+                  <p>13 Easy Tips to Reduce Plastic & Travel Plastic-Free</p>
+                </div>
+              </div>
+              <div className="row banner-article">
+                <div className="col-3">
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/7/79/Thap_Rua.jpg" alt=""/>
+                </div>
+                <div className="col-9">
+                  <p>13 Easy Tips to Reduce Plastic & Travel Plastic-Free</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- section4 -->
+      <div className="container" id="section4">
+        <div className="row">
+          <div className="col-lg-3 .col-md-3 .col-sm-6 .col-xs-12">
+            <div className="row banner-article">
+              <div className="col-3">
+                <img
+                    src="http://lh4.googleusercontent.com/-xWRKugVFBKg/VGweyxdjJqI/AAAAAAAABlI/xAJuLp8mXZI/s1600/HOI%2BAN%2B-%2BVIETNAM%2Bhoian3dagf.jpg"
+                    alt=""/>
+              </div>
+              <div className="col-9">
+                <p>13 Easy Tips to Reduce Plastic & Travel Plastic-Free</p>
+              </div>
+            </div>
+            <div className="row banner-article">
+              <div className="col-3">
+                <img
+                    src="http://lh4.googleusercontent.com/-xWRKugVFBKg/VGweyxdjJqI/AAAAAAAABlI/xAJuLp8mXZI/s1600/HOI%2BAN%2B-%2BVIETNAM%2Bhoian3dagf.jpg"
+                    alt="" />
+              </div>
+              <div className="col-9">
+                <p>13 Easy Tips to Reduce Plastic & Travel Plastic-Free</p>
+              </div>
+            </div>
+            <div className="row banner-article">
+              <div className="col-3">
+                <img
+                    src="http://lh4.googleusercontent.com/-xWRKugVFBKg/VGweyxdjJqI/AAAAAAAABlI/xAJuLp8mXZI/s1600/HOI%2BAN%2B-%2BVIETNAM%2Bhoian3dagf.jpg"
+                    alt="" />
+              </div>
+              <div className="col-9">
+                <p>13 Easy Tips to Reduce Plastic & Travel Plastic-Free</p>
+              </div>
+            </div>
+            <div className="row banner-article">
+              <div className="col-3">
+                <img
+                    src="http://lh4.googleusercontent.com/-xWRKugVFBKg/VGweyxdjJqI/AAAAAAAABlI/xAJuLp8mXZI/s1600/HOI%2BAN%2B-%2BVIETNAM%2Bhoian3dagf.jpg"
+                    alt="" />
+              </div>
+              <div className="col-9">
+                <p>13 Easy Tips to Reduce Plastic & Travel Plastic-Free</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-3 .col-md-3 .col-sm-6 .col-xs-12 section4-card">
+            <img
+                src="https://static.saltinourhair.com/wp-content/uploads/2021/06/04100711/santorini-greece-things-to-do-travel-guide-600x600.jpg"
+                alt="" />
+              <div className="bottom-left">
+                <h2>
+                  Santorini
+                </h2>
+                <h4>
+                  Greece
+                </h4>
+              </div>
+          </div>
+
+          <div className="col-lg-3 .col-md-3 .col-sm-6 .col-xs-12 section4-card">
+            <img
+                src="https://static.saltinourhair.com/wp-content/uploads/2021/06/04100711/santorini-greece-things-to-do-travel-guide-600x600.jpg"
+                alt="" />
+              <div className="bottom-left">
+                <h2>
+                  Santorini
+                </h2>
+                <h4>
+                  Greece
+                </h4>
+              </div>
+          </div>
+
+          <div className="col-lg-3 .col-md-3 .col-sm-6 .col-xs-12 section4-card">
+            <img
+                src="https://static.saltinourhair.com/wp-content/uploads/2021/06/04100711/santorini-greece-things-to-do-travel-guide-600x600.jpg"
+                alt="" />
+              <div className="bottom-left">
+                <h2>
+                  Santorini
+                </h2>
+                <h4>
+                  Greece
+                </h4>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- section5 -->
+      <div className="container" id="section5">
+        <div className="row">
+          <div className="col-lg-6 .col-md-6 .col-sm-6 .col-xs-12">
+            <img src="https://static.saltinourhair.com/build/images/home/about-us.jpg" alt="" />
+          </div>
+
+          <div className="col-lg-6 .col-md-6 .col-sm-6 .col-xs-12" id="test">
+            <div className="row about-us">
+              <div className="col-12">
+                <h1>About us</h1>
+                <p className="about-us-content">We're Hannah & Nick, two creatives that run a 'travel content
+                  creation' company named Salt in our Hair. We inspire people to travel via our travel guides,
+                  photography and videography.
+                </p>
+                <a href="#">More about us</a><span> /</span>
+                <a href="#">Work with us</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <!-- section6 -->
+      <div className="newsletter-subscribe mt-5 container">
+        <div className="container">
+          <div className="intro">
+            <h2 className="text-center newsletter">Subscribe to our Newsletter</h2>
+            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+          <form className="form-inline" method="post">
+            <div className="form-group"><input className="form-control" type="email" name="email"
+                                               placeholder="Your Email" />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-primary" type="button">Subscribe</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
